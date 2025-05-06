861a3107912cfaf78385c0d5ae097881e67ba2a7
M-Ans-Atrule <ans.khan@atrule.com>
2025-05-06 17:42:53 +0500

    added controller and gitignore changes

---
+++ ProductApi/Models/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductApi.Models
+{
+    public class Brand
+    {
+        public int id { get; set; }
+        public string domain { get; set; }
+        public string? image { get; set; }
+    }
+}
